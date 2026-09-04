10000885a77a6fddafda96fb968fe837729439cb
a.benlagha@studenti.unisa.it <Ahmed Lagha>
2026-09-04 21:16:50 +0200

    Update project

--- src/main/java/org/apache/commons/csv/ExtendedBufferedReader.java
+++ src/main/java/org/apache/commons/csv/ExtendedBufferedReader.java
@@ -19,11 +19,6 @@
 
 package org.apache.commons.csv;
 
-import static org.apache.commons.csv.Constants.CR;
-import static org.apache.commons.csv.Constants.LF;
-import static org.apache.commons.csv.Constants.UNDEFINED;
-import static org.apache.commons.io.IOUtils.EOF;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.CharBuffer;
@@ -31,7 +26,11 @@ import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 
+import static org.apache.commons.csv.Constants.CR;
+import static org.apache.commons.csv.Constants.LF;
+import static org.apache.commons.csv.Constants.UNDEFINED;
 import org.apache.commons.io.IOUtils;
+import static org.apache.commons.io.IOUtils.EOF;
 import org.apache.commons.io.input.UnsynchronizedBufferedReader;
 
 /**
--- src/test/java/org/apache/commons/csv/JiraCsv196Test.java
+++ src/test/java/org/apache/commons/csv/JiraCsv196Test.java
@@ -19,13 +19,12 @@
 
 package org.apache.commons.csv;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class JiraCsv196Test {
